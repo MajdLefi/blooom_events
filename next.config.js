093f7809d716955fbaa63c64b596943cc5427ec1
MajdLefi <majd.lefi@esprit.tn>
2024-01-05 03:05:48 +0100

    add optimization tricks

--- next.config.js
+++ next.config.js
@@ -3,12 +3,17 @@
  */
 module.exports = {
   content: [
+    "./components/**/*.{ts,tsx}",
     "./pages/**/*.{ts,tsx}",
     "./public/**/*.html",
   ],
   plugins: [
     require("flowbite/plugin")
   ],
+  // images: {
+  //   loader: 'custom',
+  //   loaderFile: './my-loader.ts',
+  // },
   images: {
     remotePatterns: [
       {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import Image from 'next/image'
 import dynamic from "next/dynamic";
 import Head from 'next/head'
-const Layout = dynamic(() => import("../components/layout"), { ssr: false });
-const HeroSection = dynamic(() => import("../components/main-page/HeroSection"), { ssr: false });
-const ContactBar = dynamic(() => import("../components/main-page/ContactBar"), { ssr: false });
-const AboutUs = dynamic(() => import("../components/main-page/AboutUs"), { ssr: false });
-const Album = dynamic(() => import("../components/main-page/Album"), { ssr: false });
-const Services = dynamic(() => import("../components/main-page/Services"), { ssr: false });
-const Team = dynamic(() => import("../components/main-page/Team"), { ssr: false });
-const WhyUs = dynamic(() => import("../components/main-page/WhyUs"), { ssr: false });
-const SocialMedia = dynamic(() => import("../components/main-page/SocialMedia"), { ssr: false });
-const Shooting = dynamic(() => import("../components/main-page/services/Shooting"), { ssr: false });
-const ContactUs = dynamic(() => import("../components/main-page/Contact"), { ssr: false });
+const Layout = dynamic(() => import("@components/layout"), { ssr: false });
+const HeroSection = dynamic(() => import("@components/main-page/HeroSection"), { ssr: false });
+const ContactBar = dynamic(() => import("@components/main-page/ContactBar"), { ssr: false });
+const AboutUs = dynamic(() => import("@components/main-page/AboutUs"), { ssr: false });
+const Album = dynamic(() => import("@components/main-page/Album"), { ssr: false });
+const Services = dynamic(() => import("@components/main-page/Services"), { ssr: false });
+const Team = dynamic(() => import("@components/main-page/Team"), { ssr: false });
+const WhyUs = dynamic(() => import("@components/main-page/WhyUs"), { ssr: false });
+const SocialMedia = dynamic(() => import("@components/main-page/SocialMedia"), { ssr: false });
+const Shooting = dynamic(() => import("@components/main-page/services/Shooting"), { ssr: false });
+const ContactUs = dynamic(() => import("@components/main-page/Contact"), { ssr: false });
 
 import { Inter } from 'next/font/google'
 
