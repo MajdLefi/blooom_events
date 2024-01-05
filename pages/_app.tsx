d86efde09a804248e16fc5c3ac0bef740c137929
MajdLefi <majd.lefi@esprit.tn>
2024-01-05 03:39:30 +0100

    fix google analytics issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-68KCW96EV2"
       />
 
       <Script id="google-analytics-script" strategy="lazyOnload">
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          gtag('config', G-68KCW96EV2 , {
           page_path: window.location.pathname,
           });
     `}
