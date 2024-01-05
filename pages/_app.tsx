1f19bfff13946918c54f138b92ed5581c22f11e7
MajdLefi <majd.lefi@esprit.tn>
2024-01-05 03:30:45 +0100

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from "next/script";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+
+      <Script id="google-analytics-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+          page_path: window.location.pathname,
+          });
+    `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
